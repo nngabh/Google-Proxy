e6dc65841d3f9d20dffaf4185366a30383f3e066
Samuel Fekadu <samft99@gmail.com>
2016-03-08 17:30:19 -0500

    Combination
    We added all the scripts together

--- Main Script.php
+++
@@ -1,8 +0,0 @@
-<?php
-$page = file ($url);
-foreach($page as $part)
-{
-$part = ereg_replace('<img src=','<img src='.$url,$part) ;
-$part = ereg_replace('action="','action="'.$url,$part);
-echo $part;
-?>
